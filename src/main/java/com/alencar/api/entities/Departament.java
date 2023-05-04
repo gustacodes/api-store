9ba12c2e4279c843f370fd42bd7ecd300764176d
Gustavo Alencar <gustavorique@live.com>
2023-05-04 02:39:55 -0300

    finalizado

--- src/main/java/com/alencar/api/entities/Departament.java
+++ src/main/java/com/alencar/api/entities/Departament.java
@@ -1,6 +1,5 @@
 package com.alencar.api.entities;
 
-
 public class Departament {
     
     private Long id;
@@ -9,13 +8,11 @@ public class Departament {
     public Departament() {
 
     }
-    
+
     public Departament(Long id, String nome) {
         this.id = id;
         this.nome = nome;
     }
-
-
 
     public Long getId() {
         return id;
